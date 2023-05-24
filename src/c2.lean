0c013e61054a5aac0415f16e0f9f4ea05e3ce395
Stavan Jain <stavanjain@Stavans-MacBook-Air.local>
2023-05-24 12:13:12 -0400

    finished c2
    dine

--- src/c2.lean
+++ src/c2.lean
@@ -11,7 +11,13 @@ def is_lub (S : set ℝ) (l : ℝ) : Prop := is_least (upper_bounds S) l
 /-- A set has at most one least upper bound -/
 theorem challenge2 (S : set ℝ) (a b : ℝ) (ha : is_lub S a) (hb : is_lub S b) : a = b :=
 begin
-  sorry
+  cases ha with uba luba, 
+  cases hb with ubb lubb, 
+  have i := luba b, /- Using the fact that a is least upper bound, we can say that b ≥ a if b is an upper bound (which it is!)-/
+  have j := lubb a, /- Same thing tells us that a ≥ b -/
+  have k:= i ubb, 
+  have l := j uba, 
+  linarith, 
 end 
 
 end challenges
