f17a7e1500a418d827c485c73d2edf4d0fe2240f
Stavan Jain <stavanjain@Stavans-MacBook-Air.local>
2023-05-24 14:27:51 -0400

    finished 4

--- src/challenge4.lean
+++ src/challenge4.lean
@@ -4,5 +4,11 @@ open function
 
 theorem challenge4 (X Y Z : Type) (f : X → Y) (g : Y → Z) : surjective (g ∘ f) → surjective g :=
 begin
-  sorry
+  unfold surjective, 
+  intro h, 
+  intro b, -- let b be arbitrary
+  have hb := h b, -- this gives us a witness
+  cases hb with a gfa, --extracting the witness
+  use f a, 
+  exact gfa, 
 end
