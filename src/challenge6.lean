8c5a1f8645758da8d8b24d27a0bf351689bafdcf
Stavan Jain <stavanjain@Stavans-MacBook-Air.local>
2023-05-24 16:00:41 -0400

    finished 7

--- src/challenge6.lean
+++ src/challenge6.lean
@@ -2,9 +2,20 @@ import data.set.basic
 
 open set
 
-def equivalence_class {X : Type} (R : X → X → Prop) (x : X) := {y : X | R x y}
+def equivalence_class {X : Type} (R : X → X → Prop) 
+(x : X) := {y : X | R x y}
 
 example (X : Type) (R : X → X → Prop) (hR : equivalence R) (x : X) : equivalence_class R x ≠ ∅ :=
 begin
-  sorry
+  unfold equivalence at hR, 
+  unfold equivalence_class , 
+  have r:= hR.1, 
+  have i := R x , 
+  have k := r x, 
+  have g:=  nonempty_iff_ne_empty, 
+  cases g, 
+  apply g_mp, use x, apply k, 
+  --apply nonempty.ne_empty, use x, apply k, 
 end
+
+
--- src/challenge7.lean
+++ src/challenge7.lean
@@ -4,7 +4,8 @@ import data.rat.basic tactic
 
 theorem some_reciprocal_is_zero : ∃ x : ℚ, 1 / x = 0 :=
 begin
-  sorry
+  use 0,  
+  norm_num,  
 end
 
 theorem no_reciprocal_is_zero : ¬ (∃ x : ℚ, 1 / x = 0) :=
